e23f5d6125baf3042a81928bc2dc153843ed2f5a
Syafiq <syafiq.mastor@ipay88.com.my>
2020-03-22 15:37:51 +0800

    Initial Commit

---
+++ Hafiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hafiz
+//
+//  Created by Syafiq Mastor on 22/03/2020.
+//  Copyright © 2020 syafiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
